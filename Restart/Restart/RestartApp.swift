42f5275f1e9418b234b9a5977a31b2ae061cabda
Sheep1sik <bohg6799@naver.com>
2024-01-28 23:54:13 +0900

    Feat: 프로젝트 생성

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by 양원식 on 2024/01/28.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
