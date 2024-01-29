4a81f1f2ce3f3794c1d357b0f06a624460a00acd
Sheep1sik <bohg6799@naver.com>
2024-01-29 19:59:28 +0900

    Feat: Set Up 설정 및 파일 생성

---
+++ Restart/Restart/Screens/HomeView.swift
@@ -0,0 +1,35 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by 양원식 on 2024/01/29.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    // MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Home")
+                .font(.largeTitle)
+            Button (action: {
+                // Some action
+                isOnboardingViewActive = true
+            }) {
+                Text("Restart")
+            }
+        } //: VSTACK
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -0,0 +1,36 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by 양원식 on 2024/01/29.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Onboarding")
+                .font(.largeTitle)
+            
+            Button(action: {
+                // Some action
+                isOnboardingViewActive = false
+            }) {
+                Text("Start")
+            }
+        } //: VSTACK
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
